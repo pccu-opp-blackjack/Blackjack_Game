f22c5f04eb98b4ee5cdf3a367202dbb4a68e560a
justin893613-star <justin893613@gmail.com>
2026-04-17 19:20:45 +0800

    Refactor Card class to use std namespace

--- Blackjack_Game/Card.h
+++ Blackjack_Game/Card.h
@@ -2,21 +2,22 @@
 #define CARD_H
 #include <iostream>
 #include <string>
+using namespace std;
 
 class Card {
 private:
-    std::string suit;
-    std::string rank;
+    string suit;
+    string rank;
     int value;
 
 public:
-    Card(std::string s, std::string r, int v) : suit(s), rank(r), value(v) {}
+    Card(string s, string r, int v) : suit(s), rank(r), value(v) {}
     
     int getValue() const { return value; }
-    std::string getRank() const { return rank; }
+    string getRank() const { return rank; }
     
     void display() const { 
-        std::cout << "[" << suit << rank << "] "; 
+        cout << "[" << suit << rank << "] "; 
     }
 };
 #endif
