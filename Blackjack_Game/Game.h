e7b2960a7ece0857a964c3b83c5e211ddb194b99
justin893613-star <justin893613@gmail.com>
2026-04-17 19:23:52 +0800

    Update Game.h

--- Blackjack_Game/Game.h
+++ Blackjack_Game/Game.h
@@ -3,6 +3,7 @@
 #include "Deck.h"
 #include "Player.h"
 #include <iostream>
+using namespace std;
 
 class Game {
 private:
@@ -28,16 +29,16 @@ public:
         std::cout << "\n====================\n";
         dealer.showPartialHand();
         player.showHand();
-        std::cout << "====================\n";
+        cout << "====================\n";
 
         if (player.getScore() == 21) {
-            std::cout << "玩家直接獲得 21 點！\n";
+            cout << "玩家直接獲得 21 點！\n";
         } else {
             // 玩家回合
             char choice;
             while (!player.isBusted()) {
-                std::cout << "要加牌嗎？(y/n): ";
-                std::cin >> choice;
+                cout << "要加牌嗎？(y/n): ";
+                cin >> choice;
                 if (choice == 'y' || choice == 'Y') {
                     player.addCard(deck.dealCard());
                     player.showHand();
@@ -48,29 +49,29 @@ public:
         }
 
         if (player.isBusted()) {
-            std::cout << "玩家爆牌！莊家獲勝。\n";
+            cout << "玩家爆牌！莊家獲勝。\n";
             return;
         }
 
         // 莊家回合 (規則：未滿 17 點必須加牌)
-        std::cout << "\n--- 莊家回合開始 ---\n";
+        cout << "\n--- 莊家回合開始 ---\n";
         dealer.showHand();
         while (dealer.getScore() < 17) {
-            std::cout << "莊家加牌...\n";
+            cout << "莊家加牌...\n";
             dealer.addCard(deck.dealCard());
             dealer.showHand();
         }
 
         // 結算
-        std::cout << "\n【最終結算】\n";
+        cout << "\n【最終結算】\n";
         if (dealer.isBusted()) {
-            std::cout << "莊家爆牌！玩家獲勝。\n";
+            cout << "莊家爆牌！玩家獲勝。\n";
         } else {
             int pScore = player.getScore();
             int dScore = dealer.getScore();
             if (pScore > dScore) std::cout << "玩家獲勝！\n";
-            else if (pScore < dScore) std::cout << "莊家獲勝！\n";
-            else std::cout << "雙方平手！\n";
+            else if (pScore < dScore) cout << "莊家獲勝！\n";
+            else cout << "雙方平手！\n";
         }
     }
 };
